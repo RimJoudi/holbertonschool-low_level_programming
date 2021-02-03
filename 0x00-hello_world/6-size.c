353797d2ed6d8a06170ef6599801c7aea4fd92bd
vagrant <vagrant@vagrant.vm>
2021-02-03 18:35:32 +0000

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
 int main(void)
-{printf("Size of char: %d byte\n", sizeof(char));
-printf("Size of int: %d bytes\n", sizeof(int));
-printf("Size of long int: %d bytes\n", sizeof(long int));
-printf("Size of long long int: %d bytes\n", sizeof(long long int));
-printf("Size of float: %d bytes\n", sizeof(float));
+{printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
