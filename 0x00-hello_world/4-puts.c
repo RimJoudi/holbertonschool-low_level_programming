b3edcab65ac96a252c44b04370f748332fd04629
vagrant <vagrant@vagrant.vm>
2021-02-03 14:28:40 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-puts "\"Programming is like building a multilingual puzzle" return(0)
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main ()
+{
+    puts("\"Programming is like building a multilingual puzzle");
+
+    return (0);
+}
