a77b7b9de418bc5739302d8341a99a931b3283d6
vagrant <vagrant@vagrant.vm>
2021-02-03 17:31:51 +0000

    size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*
+*/
+int main(void)
+{printf("Size of char: %ld byte\n", sizeof(char));
+printf("Size of int: %ld bytes\n", sizeof(int));
+printf("Size of long int: %ld bytes\n", sizeof(long int));
+printf("Size of long long int: %ld bytes\n", sizeof(long long int));
+printf("Size of float: %ld bytes\n", sizeof(float));
+return (0);
+}
