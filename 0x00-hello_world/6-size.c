9bd35d01f88d12e0a711d06f9c39d0e2bbe6b740
vagrant <vagrant@vagrant.vm>
2021-02-03 17:35:40 +0000

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 *
 */
 int main(void)
-{printf("Size of char: %ld byte\n", sizeof(char));
-printf("Size of int: %ld bytes\n", sizeof(int));
-printf("Size of long int: %ld bytes\n", sizeof(long int));
-printf("Size of long long int: %ld bytes\n", sizeof(long long int));
-printf("Size of float: %ld bytes\n", sizeof(float));
+{printf("Size of char: %d byte\n", sizeof(char));
+printf("Size of int: %d bytes\n", sizeof(int));
+printf("Size of long int: %d bytes\n", sizeof(long int));
+printf("Size of long long int: %d bytes\n", sizeof(long long int));
+printf("Size of float: %d bytes\n", sizeof(float));
 return (0);
 }
