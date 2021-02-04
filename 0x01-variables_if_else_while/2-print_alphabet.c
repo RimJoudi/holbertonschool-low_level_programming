50d3ef3a36ce83f578ed4a52f22557521ec5b47e
vagrant <vagrant@vagrant.vm>
2021-02-04 17:48:06 +0000

    print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,17 @@
-#include <stdlib.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-{char c;
-c = 'a';
-while (c <= 'z')
-{putchar(c);
-c++;
+{
+char i;
+i = 'a';
+while (i <= 'z')
+{
+putchar(i);
+i++;
 }
 putchar('\n');
 return (0);
