9570b143b4de1563686e72caa509a99366ef5c72
vagrant <vagrant@vagrant.vm>
2021-02-04 22:38:38 +0000

    print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar ('\n');
+return (0);
+}
