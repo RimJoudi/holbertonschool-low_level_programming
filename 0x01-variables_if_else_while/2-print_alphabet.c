974f982572e43920d7f687910596d965834348b1
vagrant <vagrant@vagrant.vm>
2021-02-04 17:37:39 +0000

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{char c;
+c = 'a';
+while (c <= 'z')
+{putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
