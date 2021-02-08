3d2dd82c040f3b744c1426d985c7f8f600c534da
vagrant <vagrant@vagrant.vm>
2021-02-08 23:42:08 +0000

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{int i;
+char j;
+for(i = 0; i <= 10; i++){
+for(j ='a'; j <= 'z'; j++)
+_putchar(j);
+_putchar('\n');
+}
+}
