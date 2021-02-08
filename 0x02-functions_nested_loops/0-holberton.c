23d3aa980cd6cff39cbc1ca14a1bcc392b38ee78
vagrant <vagrant@vagrant.vm>
2021-02-08 13:48:43 +0000

    holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <holberton.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+_ putchar('H');
+_ putchar('o');
+_ putchar('l');
+_ putchar('b');
+_ putchar('e');
+_ putchar('r');
+_ putchar('t');
+_ putchar('o');
+_ putchar('n');
+_ putchar('\n');
+return (0);
+};
