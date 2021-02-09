942d98ea60b26965a47a558f274581350d409738
vagrant <vagrant@vagrant.vm>
2021-02-09 22:14:45 +0000

    origin master

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a > b)
+		largest = a;
+	else
+		largest = b;
+
+	if (c > largest)
+		largest = c;
+
+	return (largest);
+}
