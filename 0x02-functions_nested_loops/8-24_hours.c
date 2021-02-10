34e754039ae6f646b6adbcc23ba53ace946e3c69
RimJoudi <vagrant@vagrant.vm>
2021-02-10 16:14:46 +0000

    24h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * jack_bauer -  prints every minute of the day.
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{int i, j;
+
+for (i = 0; i <= 23 ; i++)
+{
+for (j = 0; j <= 59; j++)
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+_putchar('\n');
+}
+}
+}
