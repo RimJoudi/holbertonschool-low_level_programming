c58bb21be7f6cdb8696290b99556c55cfb2b6fe3
RimJoudi <vagrant@vagrant.vm>
2021-02-10 15:44:02 +0000

    last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *print_last_digit - 0
+  *@n: integer
+  *
+  *Return: last digit
+  */
+int print_last_digit(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+			i = (n % 10) * (-1);
+			_putchar(i + '0');
+	return (i);
+	}
+	else
+	{
+			i = n % 10;
+			_putchar(i + '0');
+	return (i);
+	}
+}
