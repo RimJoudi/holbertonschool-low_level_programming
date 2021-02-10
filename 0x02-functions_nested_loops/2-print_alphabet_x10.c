e6ad23cc8905010116f11e79dd5a558a0a8d8244
RimJoudi <vagrant@vagrant.vm>
2021-02-10 13:00:54 +0000

    print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,20 @@
 #include "holberton.h"
+
 /**
- * print_alphabet_x10 - print alphabet
+ * print_alphabet_x10 - show10linesofalphabet
  *
- * Return;
+ * Return: Always 0.
  */
 void print_alphabet_x10(void)
-{
-	int i, j;
+{	char c;
+	int i;
 
-	for (i = 0; i < 10; i++)
-
+	for (i = 1; i <= 10; ++i)
 		{
-			for (j = 97; j <= 122; j++)
+
+			for (c = 'a' ; c <= 'z'; c++)
 				{
-					_putchar(j);
+					_putchar(c);
 				}
 			_putchar('\n');
 		}
