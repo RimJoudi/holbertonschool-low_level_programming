3a18cd285b33a03a0eb90656ed646f5150f874bb
RimJoudi <vagrant@vagrant.vm>
2021-02-10 15:49:01 +0000

    last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,18 +7,17 @@
   */
 int print_last_digit(int n)
 {
-	int i;
-
-	if (n < 0)
-	{
-			i = (n % 10) * (-1);
-			_putchar(i + '0');
-	return (i);
-	}
-	else
-	{
-			i = n % 10;
-			_putchar(i + '0');
-	return (i);
-	}
+int i;
+if (n < 0)
+{
+i = (n % 10) * (-1);
+_putchar(i + '0');
+return (i);
+}
+else
+{
+i = n % 10;
+_putchar(i + '0');
+return (i);
+}
 }
