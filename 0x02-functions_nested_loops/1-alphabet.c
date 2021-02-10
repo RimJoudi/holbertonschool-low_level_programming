4acb035b3d158edd263a93afd29dc86c9213c45e
RimJoudi <vagrant@vagrant.vm>
2021-02-10 10:27:50 +0000

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 void print_alphabet(void)
 {char i;
-for(i = 'a'; i <= 'z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 _putchar(i);
 _putchar('\n');
 }
