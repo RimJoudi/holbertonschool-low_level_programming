589c68f55fa4a1965a9f4217026336cec42d15b4
RimJoudi <vagrant@vagrant.vm>
2021-02-11 10:16:23 +0000

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints the numbers.
+ *
+ * Return: nothing.
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 48; i <= 57 ; i++)
+{
+_putchar (i);
+}
+_putchar ('\n');
+}
