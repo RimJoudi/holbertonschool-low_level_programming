ef7ea357d7fc47c84781524eb2bc3bf8f44be32c
RimJoudi <vagrant@vagrant.vm>
2021-02-11 14:54:03 +0000

    print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_diagonal - print a diagonal line
+ *@n:integer.
+ *Return: nothing.
+ */
+void print_diagonal(int n)
+{
+int i, j;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}
