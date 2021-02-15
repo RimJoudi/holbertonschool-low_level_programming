ff19bc5ec1505fda347b2f6d3f47309f70ab31d2
RimJoudi <vagrant@vagrant.vm>
2021-02-15 13:48:59 +0000

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *_strlen - returns the length of a string.
+ *@s: charachter.
+ * Return: length of a string.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
