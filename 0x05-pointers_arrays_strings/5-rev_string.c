238511356f16b9aac34231d9bcab3ec43b383f31
RimJoudi <vagrant@vagrant.vm>
2021-02-16 14:13:54 +0000

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * rev_string - reverses a string.
+ *@s:string.
+ *Return: nothing.
+ */
+void rev_string(char *s)
+{
+	char c;
+	int i, n;
+
+	for (i = 0; s[i + 1] != 0; i++)
+	{
+		continue;
+	}
+	n = i;
+	i = 0;
+	while (i < n / 2 + 1)
+	{
+		c = s[i];
+		s[i] = s[n - i];
+		s[n - i] = c;
+		i++;
+	}
+}
