9fe02b4e26f6db1fed9f662dd9b2ddf26c162f1f
RimJoudi <vagrant@vagrant.vm>
2021-02-18 20:22:29 +0000

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,16 +12,16 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 
 	i = 0;
-	while (dest[i] != '\0')
+	while (dest[i] != 0)
 	{
 		i++;
 	}
 	j = 0;
-	while (src[j] != '\0')
+	while (src[j] != 0 && j < n)
 	{
-		n = src[0];
-		src[0] = dest [i];
-		dest [i] = n;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 	return (dest);
 }
