19a1185fe5b0a28c848483d628a5197103df3f8e
RimJoudi <vagrant@vagrant.vm>
2021-02-22 21:37:04 +0000

    set_string

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *set_string - sets the value of a pointer to a char.
+ *@s: char.
+ *@to: char.
+ *return: nothing.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
