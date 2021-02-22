02ef03caf098851e78b17face3aad1a07e8f7c3b
RimJoudi <vagrant@vagrant.vm>
2021-02-22 21:02:48 +0000

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+*_strstr - finds the first occurrence of a substring in a string
+* @haystack:char
+* @needle:char
+* Return:a pointer or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] != '\0')
+		{
+			if (haystack[i] == needle[j])
+			return (&haystack[i]);
+			i++;
+		}
+		j++;
+	}
+	return (0);
+}
