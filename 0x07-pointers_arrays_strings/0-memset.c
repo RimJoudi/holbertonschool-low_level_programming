b68ef8ca5f2ade7e074da6879d7d3255eb974f47
RimJoudi <vagrant@vagrant.vm>
2021-02-22 16:40:07 +0000

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _memset - fill memory with a constant byte
+ * @s:char
+ * @b:char
+ * @n:unsigned int
+ * Return:char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
