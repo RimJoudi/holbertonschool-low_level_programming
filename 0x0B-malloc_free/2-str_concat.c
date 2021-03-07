d40476a2af0a153edad6ecf3f35be65353fffbca
RimJoudi <vagrant@vagrant.vm>
2021-03-07 14:43:55 +0000

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,29 +10,30 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-unsigned int i, j, l1, l2;
-char *p;
-if (s1 == NULL)
-l1 = 0;
-else
-{
-for (l1 = 0; s1[l1] != '\0'; l1++)
-continue;
-}
-if (s2 == NULL)
-l2 = 0;
-else
-{
-for (l2 = 0; s2[l2] != '\0'; l2++)
-continue;
-}
-p = malloc(sizeof(char) * (l1 + l2 + 1));
-if (p == NULL)
-return (0);
-for (i = 0; i < l1; i++)
-p[i] = s1[i];
-for (j = 0; j < l2; j++)
-p[l1 + j] = s2[j];
-p[l1 + l2] = '\0';
-return (p);
+	unsigned int i, j, l1, l2;
+	char *p;
+
+	if (s1 == NULL)
+		l1 = 0;
+	else
+	{
+		for (l1 = 0; s1[l1] != '\0'; l1++)
+			continue;
+	}
+	if (s2 == NULL)
+		l2 = 0;
+	else
+	{
+		for (l2 = 0; s2[l2] != '\0'; l2++)
+			continue;
+	}
+	p = malloc(sizeof(char) * (l1 + l2 + 1));
+	if (p == NULL)
+		return (0);
+	for (i = 0; i < l1; i++)
+		p[i] = s1[i];
+	for (j = 0; j < l2; j++)
+		p[l1 + j] = s2[j];
+	p[l1 + l2] = '\0';
+	return (p);
 }
