17ccef28ec9be4cffd24106ee5e285b50b152842
RimJoudi <joudirim93@gmail.com>
2021-03-07 21:40:18 +0000

    free-grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,8 +10,9 @@
  */
 void free_grid(int **grid, int height)
 {
-int i;
-for (i = 0; i < height; i++)
-free(grid[i]);
-free(grid);
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
 }
