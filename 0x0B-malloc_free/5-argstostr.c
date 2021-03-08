67d303494601d84371ffbfba9293ce79d3ebcf67
RimJoudi <joudirim93@gmail.com>
2021-03-08 00:17:50 +0000

    argstostr

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -10,27 +10,28 @@
  */
 char *argstostr(int ac, char **av)
 {
-int i, j, x = 0, l = 0;
-char *p;
-if ((ac == 0) || (av == NULL))
-return (NULL);
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j] != '\0'; j++)
-l++;
-}
-p = malloc(sizeof(char) * (ac + l + 1));
-if (p == NULL)
-return (NULL);
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j] != '\0'; j++)
-{
-p[x] = av[i][j];
-x++;
-}
-p[x++] = '\n';
-}
-p[x] = '\0';
-return (p);
+	int i, j, x = 0, l = 0;
+	char *p;
+
+	if ((ac == 0) || (av == NULL))
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			l++;
+	}
+	p = malloc(sizeof(char) * (ac + l + 1));
+		if (p == NULL)
+			return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			p[x] = av[i][j];
+			x++;
+		}
+		p[x++] = '\n';
+	}
+	p[x] = '\0';
+	return (p);
 }
