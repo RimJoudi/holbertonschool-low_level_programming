ce66e102ed8278abe8a3eb84953f5f1c0dd8e6ce
RimJoudi <joudirim93@gmail.com>
2021-03-09 22:40:46 +0000

    header file def_new_type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,6 +12,10 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
