68b27c490e7aa645c6cea71ecf1252b85ebec3ab
RimJoudi <joudirim93@gmail.com>
2021-03-10 22:48:03 +0000

    int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,16 +10,17 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i, k;
-if (array == NULL || cmp == NULL)
-return (-1);
-if (size <= 0)
-return (-1);
-for (i = 0; i < size; i++)
-{
-k = cmp(array[i]);
-if (k == 1)
-return (i);
-}
-return (-1);
+	int i, k;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		k = cmp(array[i]);
+		if (k == 1)
+			return (i);
+	}
+	return (-1);
 }
