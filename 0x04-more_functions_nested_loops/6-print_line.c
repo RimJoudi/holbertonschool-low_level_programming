68aadda787a92e4e45c3d7d0a13e67ed0c3c0428
RimJoudi <joudirim93@gmail.com>
2021-03-13 15:22:38 +0000

    print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,15 +7,16 @@
  */
 void print_line(int n)
 {
-int i;
-i = 0;
-if (n > 0)
-{
-while (i < n)
-{
-_putchar('_');
-i++;
-}
-}
-_putchar('\n');
+	int i;
+
+	i = 0;
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+		}
+	_putchar('\n');
 }
