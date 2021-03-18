b1a0adb35d15dfdebc8ff20387a001bfcb5ef080
RimJoudi <joudirim93@gmail.com>
2021-03-18 22:44:48 +0000

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * f_print - prints a sentence before the main function is executed.
+ *
+ *Return: nothing.
+ */
+
+void f_print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
