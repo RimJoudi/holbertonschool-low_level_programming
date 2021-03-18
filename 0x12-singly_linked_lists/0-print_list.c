0cade24464bded239458698eac2dce2402742d06
RimJoudi <joudirim93@gmail.com>
2021-03-18 19:46:11 +0000

    print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,11 +17,11 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-			len++;
+		h = h->next;
+		len++;
 	}
 	return (len);
 }
