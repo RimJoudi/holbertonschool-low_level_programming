7435c27d7c3d2bc852bbf5cc7fdc37f5bef6f20b
RimJoudi <joudirim93@gmail.com>
2021-03-23 15:23:00 +0000

    pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,16 +11,14 @@
  */
 int pop_listint(listint_t **head)
 {
-listint_t *tmp;
-int n;
-if (*head == NULL)
-return (0);
-while (*head != NULL)
-{
-tmp = *head;
-*head = (*head)->next;
-n = tmp->n;
-free(tmp);
-}
-return (n);
+	listint_t *tmp;
+	int n;
+
+	if (*head == NULL)
+		return (0);
+	tmp = *head;
+	*head = (*head)->next;
+	n = tmp->n;
+	free(tmp);
+	return (n);
 }
