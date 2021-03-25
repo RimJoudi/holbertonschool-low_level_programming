5afc4678d73900a849a95a6157f844745a9239d1
RimJoudi <joudirim93@gmail.com>
2021-03-25 21:14:26 +0000

    endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * get_endianness - converts a binary number to an unsigned int.
+ *
+ * Returns: 0 if big endian otherwise 1 if little endian.
+ */
+int get_endianness(void)
+{
+	unsigned int i;
+	char *c = (char *)&i;
+
+	i = 1;
+	if (*c != '\0')
+		return (0);
+	else
+		return (1);
+}
