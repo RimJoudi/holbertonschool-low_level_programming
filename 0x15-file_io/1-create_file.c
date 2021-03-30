f53c5d31b0b8128483f3a9266207da5498ca8490
RimJoudi <joudirim93@gmail.com>
2021-03-30 21:04:58 +0000

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  *create_file - creates a file.
  *@filename: name of the file to create char.
- *@text_content: string 
+ *@text_content: content of the file string.
  *Return: 1 on success otherwise -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
@@ -16,15 +16,19 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	
-	for (len = 0; text_content[len] != '\0'; len++)
-		continue;
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+			continue;
 
-	wr = write (fd, text_content, len);
-	if (wr == -1)
-		return (-1);
+		wr = write(fd, text_content, len);
+		if (wr == -1)
+		{
+			return (-1);
+		}
+	}
 
-	close (fd);
+	close(fd);
 
 	return (1);
 }
