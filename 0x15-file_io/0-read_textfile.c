5664bf6af13cbf275da929beb0e5e6423a86f82d
RimJoudi <joudirim93@gmail.com>
2021-03-30 19:26:14 +0000

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,16 +22,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	rd = read (fd, buff, letters);
+	rd = read(fd, buff, letters);
 	if (rd == -1)
 		return (0);
 
-	wr = write (STDOUT_FILENO, buff, rd);
+	wr = write(STDOUT_FILENO, buff, rd);
 	if (wr == -1)
 		return (0);
 
-	close (fd);
-	free (buff);
+	close(fd);
+	free(buff);
 
 	return (wr);
 }
