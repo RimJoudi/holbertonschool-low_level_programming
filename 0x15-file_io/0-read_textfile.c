9bc88e620649d268a69e87e3d54f772adc4502bd
RimJoudi <joudirim93@gmail.com>
2021-03-30 19:20:49 +0000

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- *read_textfile -  reads a text file and prints it to the POSIX standard output.
- *@filename: file to read and print.
- *@letters:  number of letters it should read and print.
+ *read_textfile - reads a text file and prints it to the POSIX standard output.
+ *@filename: file to read and print of type char.
+ *@letters: number of letters of type size_t.
  *Return: number of letters it could read and print otherwise 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -26,12 +26,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (rd == -1)
 		return (0);
 
-	wr = write (fd, buff, rd);
+	wr = write (STDOUT_FILENO, buff, rd);
 	if (wr == -1)
 		return (0);
 
 	close (fd);
 	free (buff);
 
-	return (letters);
+	return (wr);
 }
