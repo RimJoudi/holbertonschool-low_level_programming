8bf91b119865ba45b43fd640f37ebe6741aae0ba
RimJoudi <joudirim93@gmail.com>
2021-03-30 18:51:37 +0000

    header file

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,5 +1,11 @@
 #ifndef HEADR_FILE
 #define HEADR_FILE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unisted.h>
+#include <sys/types>
+#include <sys/stat.h>
+#include <fcntl.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
