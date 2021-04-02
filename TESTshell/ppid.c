c8f256530a0bc124f7b0b4858f771c720eede3b7
RimJoudi <joudirim93@gmail.com>
2021-04-02 14:11:44 +0000

    test

---
+++ TESTshell/ppid.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - PPID
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	pid_t my_ppid;
+
+	my_ppid = getppid();
+	printf("%u\n", my_ppid);
+	return (0);
+}
