ea349cb4862a2158478211d2b6f86d2c7feb19c5
RimJoudi <joudirim93@gmail.com>
2021-05-28 22:33:19 +0100

    commit to task4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ *hash_table_get -  retrieves a value associated with a key.
+ *@ht:  hash table pointer
+ *@key: the key 
+ *Return:  associated value element, otherwise, NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
+	if (node == NULL)
+		return (NULL);
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
