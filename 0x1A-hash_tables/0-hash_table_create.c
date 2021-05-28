c04bcd175aa47fb5f89999242ca020d0b4fb54e9
RimJoudi <joudirim93@gmail.com>
2021-05-28 13:57:52 +0100

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,14 +10,17 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *h_tab;
+if (size == 0)
+return (NULL);
 h_tab = malloc(sizeof(hash_table_t));
 if (h_tab == NULL)
-return (NULL);
-if (size == 0)
 return (NULL);
 h_tab->size = size;
 h_tab->array = malloc(sizeof(hash_node_t *) * size);
 if (h_tab->array == NULL)
+{
+free(h_tab);
 return (NULL);
+}
 return (h_tab);
 }
