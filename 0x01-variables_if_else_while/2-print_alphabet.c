d53a4de08518ce9ff7f28e4f299fb1bcb0fc5bdb
RimJoudi <joudirim93@gmail.com>
2021-06-19 20:52:06 +0100

    betty styling

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - prints the alphabet in lowercase.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char i;
-i = 'a';
-while (i <= 'z')
-{
-putchar(i);
-i++;
-}
-putchar('\n');
-return (0);
+	char i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
