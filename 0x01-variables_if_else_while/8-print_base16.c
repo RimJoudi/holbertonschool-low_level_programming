133af87260d03999618e6d22bfad8bb6fbd4544a
RimJoudi <joudirim93@gmail.com>
2021-06-19 18:50:38 +0100

    betty styling

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - prints all the numbers of base 16 in lowercase,
+ *  followed by a new line.
  *
- * Return: Always 0 (Success)
+ * Return:  Return: Always 0 (Success)
  */
 int main(void)
 {
-int i;
-char j;
-for (i = 48; i <= 57; i++)
-{
-putchar(i);
-}
-for (j = 'a'; j <= 'f'; j++)
-{
-putchar(j);
-}
-putchar('\n');
-return (0);
+	int i;
+	char j;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
 }
