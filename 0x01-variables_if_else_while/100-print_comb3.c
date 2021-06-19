c7fd65ea1ff83ce553dc4391ce66abed5c93db67
RimJoudi <joudirim93@gmail.com>
2021-06-19 18:26:37 +0100

    betty styling

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - prints all possible different combinations of two digits.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i;
-int j;
-for (i = 48; i < 57; i++)
-{
-for (j = 49; j <= 57; j++)
-{
-if (j > i)
-{
-putchar(i);
-putchar(j);
-if (i < 56)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	int i, j;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i < 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
