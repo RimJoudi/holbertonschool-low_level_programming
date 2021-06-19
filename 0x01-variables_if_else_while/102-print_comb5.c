db1be19f73051fe5be04117ae4894f85f3bf4ff0
RimJoudi <joudirim93@gmail.com>
2021-06-19 18:25:09 +0100

    betty styling

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - prints all possible combinations of two two-digit numbers.
  *
  * Return: Always 0 (Success)
  */
@@ -10,28 +10,28 @@ int i, j, l, k;
 
 for (i = 0; i <= 9; i++)
 {
-for (j = 0; j <= 9; j++)
-{
-for (l = 0; l <= 9; l++)
-{
-for (k = 0; k <= 9; k++)
-{
-if (i < l || (i == l && j < k))
-{
-putchar(i + '0');
-putchar(j + '0');
-putchar(' ');
-putchar(l + '0');
-putchar(k + '0');
-if (!(i == 9 && j == 8 && l == 9 && k == 9))
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
+	for (j = 0; j <= 9; j++)
+	{
+		for (l = 0; l <= 9; l++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				if (i < l || (i == l && j < k))
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(l + '0');
+					putchar(k + '0');
+					if (!(i == 9 && j == 8 && l == 9 && k == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
 }
 putchar('\n');
 return (0);
