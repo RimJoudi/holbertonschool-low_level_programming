f5fe50aab00f62f3189141ba55931618e6130c20
RimJoudi <joudirim93@gmail.com>
2021-06-19 18:55:48 +0100

    betty styling

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i;
-for (i = 48; i <= 57; i++)
-{
-putchar(i);
-}
-putchar ('\n');
-return (0);
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar ('\n');
+	return (0);
 }
