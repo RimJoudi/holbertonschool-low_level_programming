f684df21217bb5b48270ad5c0769f7345d6d26c2
RimJoudi <joudirim93@gmail.com>
2021-06-19 18:53:55 +0100

    betty styling

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - prints the lowercase alphabet in reverse, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char i;
-for (i = 'z'; i >= 'a'; i--)
-{
-putchar(i);
-}
-putchar ('\n');
-return (0);
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
+	putchar ('\n');
+	return (0);
 }
