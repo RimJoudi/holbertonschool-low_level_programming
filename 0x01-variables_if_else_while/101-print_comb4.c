7c9bd8614b13be51f9a4a7186b8100d02b6785a8
RimJoudi <joudirim93@gmail.com>
2021-06-19 18:00:38 +0100

    betty styling

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - prints all possible different combinations of three digits.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i, j, l;
-for (i = 48; i <= 57; i++)
-{
-for (j = 49; j <= 57; j++)
-{
-for (l = 50; l <= 57; l++)
-{
-if (l > j && j > i)
-{
-putchar(i);
-putchar(j);
-putchar(l);
-if (i < 55)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	int i, j, l;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			for (l = 50; l <= 57; l++)
+			{
+				if (l > j && j > i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(l);
+					if (i < 55)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
