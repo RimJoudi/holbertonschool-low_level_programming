35f94ce5141d0297d2a8557106a107c78a5ea550
RimJoudi <joudirim93@gmail.com>
2021-06-19 16:59:58 +0100

    commit to task102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j, l, k;
+
+for (i = 48; i <= 57; i++)
+{
+for (j = 48; j <= 56; j++)
+{
+for (l = 48; l <= 57; l++)
+{
+for (k = 49; k <= 57; k++)
+{
+putchar(i);
+putchar(j);
+putchar(' ');
+putchar(l);
+putchar(k);
+if (!(i == 57 && j == 56 && l == 57 && k == 57))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
