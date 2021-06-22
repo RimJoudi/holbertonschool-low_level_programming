93632c3e46733ee5e9a0966175b0488ad419f384
RimJoudi <joudirim93@gmail.com>
2021-06-23 00:23:22 +0100

    betty styling

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,23 +1,24 @@
 #include "holberton.h"
 /**
- *print_last_digit - 0
-  *@n: integer
-  *
-  *Return: last digit
-  */
+ *print_last_digit - prints the last digit of a number.
+	*@n: integer
+	*
+	*Return: last digit
+	*/
 int print_last_digit(int n)
 {
-int i;
-if (n < 0)
-{
-i = (n % 10) * (-1);
-_putchar(i + '0');
-return (i);
-}
-else
-{
-i = n % 10;
-_putchar(i + '0');
-return (i);
-}
+	int i;
+
+	if (n < 0)
+	{
+		i = (n % 10) * (-1);
+		_putchar(i + '0');
+		return (i);
+	}
+	else
+	{
+		i = n % 10;
+		_putchar(i + '0');
+		return (i);
+	}
 }
