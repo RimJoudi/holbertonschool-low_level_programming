656a0b6f1299602f2b4aa90331cfde1d49229be4
RimJoudi <joudirim93@gmail.com>
2021-06-22 23:09:27 +0100

    betty styling

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,21 @@
 #include "holberton.h"
 
 /**
- * print_alphabet_x10 - show10linesofalphabet
+ * print_alphabet_x10 - show 10 lines of lower case alphabets
  *
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
-char c;
-int i;
-for (i = 0; i < 10; ++i)
-{
-for (c = 'a' ; c <= 'z'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
-}
+	char c;
+	int i;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+		_putchar(c);
+		}
+	_putchar('\n');
+	}
 }
