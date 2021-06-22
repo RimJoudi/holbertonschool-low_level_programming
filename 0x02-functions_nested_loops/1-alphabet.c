9d1a27424a283d43d45147ffe23031f6dd7ee8f2
RimJoudi <joudirim93@gmail.com>
2021-06-22 20:38:06 +0100

    betty styling

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
 
 /**
- * print_alphabet - check the code for Holberton School students.
+ * print_alphabet - prints alphabets in lower case
  *
  * Return: Always 0.
  */
 void print_alphabet(void)
-{char i;
-for (i = 'a'; i <= 'z'; i++)
-_putchar(i);
-_putchar('\n');
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	_putchar(i);
+	_putchar('\n');
 }
