df0fb7cdb8755665629bad572c942b99697a740a
RimJoudi <joudirim93@gmail.com>
2021-06-22 23:38:03 +0100

    fix betty

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,13 +7,13 @@
  */
 int _abs(int n)
 {
-if (n > 0)
-{
-return (n);
-}
-else if (n < 0)
-{return (n = n * -1);
-}
-else
-return (0);
+    if (n > 0)
+    {
+        return (n);
+    }
+    else if (n < 0)
+    {
+        return (n = n * -1);
+    }
+    return (0);
 }
