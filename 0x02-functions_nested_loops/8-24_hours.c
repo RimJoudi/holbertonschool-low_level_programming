34fc4b28fbcb6d9473e9959b00e6d6d20e62ecaa
RimJoudi <joudirim93@gmail.com>
2021-06-23 21:19:43 +0100

    betty styling

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,18 +5,19 @@
  * Return: Always 0.
  */
 void jack_bauer(void)
-{int i, j;
+{
+	int i, j;
 
-for (i = 0; i <= 23 ; i++)
-{
-for (j = 0; j <= 59; j++)
-{
-_putchar((i / 10) + '0');
-_putchar((i % 10) + '0');
-_putchar(':');
-_putchar((j / 10) + '0');
-_putchar((j % 10) + '0');
-_putchar('\n');
-}
-}
+	for (i = 0; i <= 23 ; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
