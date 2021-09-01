6c8d7c6073c8ab87fe16ed3525291f88be7c639d
RimJoudi <joudirim93@gmail.com>
2021-09-01 00:13:03 +0000

    task0

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/* standard lib */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
